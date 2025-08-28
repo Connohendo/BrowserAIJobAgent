1cd29322b455e5f372440a7540e6ce1457c23942
devin-w-rogers <devin.w.rogers@gmail.com>
2025-08-28 14:01:38 -0500

    add base version for google gemini model

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import asyncio
 from dotenv import load_dotenv
 load_dotenv()
-from browser_use import Agent, ChatOpenAI
+from browser_use import Agent, ChatGoogle
+
+llm = ChatGoogle(model='gemini-2.5-flash')
 
 async def main():
     agent = Agent(
         task="Find the number of stars of the browser-use repo",
-        llm=ChatOpenAI(model="gpt-4.1-mini"),
+        llm=llm
     )
     await agent.run()
 
